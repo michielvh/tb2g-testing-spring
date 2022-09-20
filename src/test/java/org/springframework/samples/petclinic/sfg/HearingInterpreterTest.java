92c2a358e58ce9d19ffbfec87117917365de4d06
Michiel Van Huyck <mvanhuyck@cibg.brussels>
2022-09-20 21:07:03 +0200

    #6 : using active profiles to specify which configuration gets loaded

--- src/test/java/org/springframework/samples/petclinic/sfg/HearingInterpreterTest.java
+++ src/test/java/org/springframework/samples/petclinic/sfg/HearingInterpreterTest.java
@@ -3,11 +3,13 @@ package org.springframework.samples.petclinic.sfg;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import static org.junit.Assert.assertEquals;
 
+@ActiveProfiles("base-test")
 @RunWith(SpringRunner.class)
 // To use spring context, we need to point it to our config classes
 @ContextConfiguration(classes = {BaseConfig.class, LaurelConfig.class})
--- src/test/java/org/springframework/samples/petclinic/sfg/junit5/HearingInterpreterComponentScanTest.java
+++ src/test/java/org/springframework/samples/petclinic/sfg/junit5/HearingInterpreterComponentScanTest.java
@@ -4,17 +4,21 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 import org.springframework.samples.petclinic.sfg.HearingInterpreter;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit.jupiter.SpringJUnitConfig;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
+@ActiveProfiles("component-scan")
 @SpringJUnitConfig(classes = HearingInterpreterComponentScanTest.TestConfig.class)
 class HearingInterpreterComponentScanTest {
 
     //Now uses the components from the mainclasses to autowire HearingInterceptor and LauralWordproducer
     //vs innerclass
     // vs @SpringJUnitConfig(classes = {BaseConfig.class, LaurelConfig.class})     (laural = primary)
+    @Profile("component-scan")
     @Configuration
     @ComponentScan("org.springframework.samples.petclinic.sfg")  // goes to both java and test packages
     static class TestConfig {
