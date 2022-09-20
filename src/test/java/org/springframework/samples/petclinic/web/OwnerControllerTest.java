af52bd51a28b55ba44ee08fb6044b6a753023741
Michiel Van Huyck <mvanhuyck@cibg.brussels>
2022-09-20 23:39:05 +0200

    #10.3: aasignment Spring MVC test with params !!

--- src/test/java/org/springframework/samples/petclinic/web/OwnerControllerTest.java
+++ src/test/java/org/springframework/samples/petclinic/web/OwnerControllerTest.java
@@ -1,19 +1,38 @@
 package org.springframework.samples.petclinic.web;
 
+import java.util.ArrayList;
+import java.util.List;
+import org.assertj.core.util.Lists;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.ArgumentCaptor;
+import org.mockito.Captor;
+import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.samples.petclinic.model.Owner;
 import org.springframework.samples.petclinic.service.ClinicService;
 import org.springframework.test.context.junit.jupiter.web.SpringJUnitWebConfig;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MockMvcBuilder;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
+import net.bytebuddy.agent.builder.AgentBuilder;
+
+import static org.assertj.core.api.Java6Assertions.assertThat;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.BDDMockito.given;
+import static org.mockito.BDDMockito.then;
+import static org.mockito.Mockito.reset;
+import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.model;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
 
+@ExtendWith(MockitoExtension.class)
 @SpringJUnitWebConfig(locations = {"classpath:spring/mvc-test-config.xml", "classpath:spring/mvc-core-config.xml"})
 class OwnerControllerTest {
 
@@ -27,9 +46,20 @@ class OwnerControllerTest {
 
     MockMvc mockMvc;
 
+
+    @Captor
+    ArgumentCaptor<String> stringArgumentCaptor;
+
     @BeforeEach
     void setUp() {
         mockMvc=MockMvcBuilders.standaloneSetup(ownerController).build();
+
+
+    }
+
+    @AfterEach
+    void tearDown() {
+        reset(clinicService);
     }
 
     @Test
@@ -40,6 +70,61 @@ class OwnerControllerTest {
                 .andExpect(status().isOk())
                 .andExpect(view().name("owners/findOwners"));
     }
+
+    @Test
+    void testReturnListOfOwners() throws Exception {
+        given(clinicService.findOwnerByLastName("")).willReturn(Lists.newArrayList(new Owner(), new Owner()));
+
+        mockMvc.perform(get("/owners"))
+                .andExpect(status().isOk())
+                .andExpect(view().name("owners/ownersList"));
+
+        then(clinicService).should().findOwnerByLastName(stringArgumentCaptor.capture());
+
+        assertThat(stringArgumentCaptor.getValue()).isEqualToIgnoringCase("");
+    }
+
+
+    @Test
+    void testFindOwnerOneResult() throws Exception {
+        Owner justOne = new Owner();
+        justOne.setId(1);
+        final String findJustOne = "FindJustOne";
+
+        justOne.setLastName(findJustOne);
+
+        given(clinicService.findOwnerByLastName(findJustOne)).willReturn(Lists.newArrayList(justOne));
+
+        mockMvc.perform(get("/owners")
+                        .param("lastName", findJustOne))
+                .andExpect(status().is3xxRedirection())
+                .andExpect(view().name("redirect:/owners/1"));
+
+        then(clinicService).should().findOwnerByLastName(anyString());
+
+    }
+
+    @Test
+    void testFindByNameNull() throws Exception {
+        mockMvc.perform(get("/owners"))
+                .andExpect(status().isOk())
+                .andExpect(view().name("owners/findOwners"));
+    }
+
+    @Test
+    void testFindByNameOne() throws Exception {
+        mockMvc.perform(get("/owners"))
+                .andExpect(status().isOk())
+                .andExpect(view().name("owners/findOwners"));
+    }
+
+    @Test
+    void testFindByNameMultiple() throws Exception {
+        mockMvc.perform(get("/owners"))
+                .andExpect(status().isOk())
+                .andExpect(view().name("owners/findOwners"));
+    }
+
 
     @Test
     void initCreationForm() throws Exception {
